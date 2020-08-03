83a5a355d09355b06c1e94f91528c8c96fe2bbef
azgord <azgord1234@gmail.com>
2020-08-03 12:31:13 +0300

    Начало разработки фронта

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form>
+      <h1>Создать событие</h1>
+      <div class="form-input">
+        <input type="text" id="title">
+        <label for="title">Заголовок</label>
+      </div>
+      <div class="form-input">
+        <textarea id="description"></textarea>
+        <label for="description">Заголовок</label>
+      </div>
+      <div class="select-time">
+        <date-time-picker
+            v-model="date"
+            :inline="true"
+            :value-format="'yyyy-LL-dd HH:mm:ss'"
+        ></date-time-picker>
+        <p>Выбранное время: {{date}}</p>
+      </div>
+    </form>
+    <ul class="task-list" v-if="taskList.length > 0">
+      <li v-for="(item, index) in taskList" :key="index">{{ item }}</li>
+    </ul>
+    <p v-else>Задач еще нету</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      date: '2019-10-01 00:00:00',
+      taskList: []
+    }
+  },
+  methods: {
+    getListTasks: function () {
+      axios.get('')
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding: 0;
+  }
+  textarea {
+    resize: none;
+    width: 370px;
+    height: 100px;
+    padding: 16px;
+  }
+  .container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  .select-time {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  .form-input {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 24px;
+  }
+  ul {
+    list-style-type: none;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import DateTimePicker from 'vue-vanilla-datetime-picker';
+
+Vue.component('date-time-picker', DateTimePicker);
 
 Vue.config.productionTip = false
 
